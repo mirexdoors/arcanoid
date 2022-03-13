5f399bc90644d439d27c31c59d84a36f5c0945af
Mikhail Prokudin <mirex.doors@mail.ru>
2022-03-13 16:02:27 +0700

    add collision detection

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+const brickRowCount = 3;
+const brickColumnCount = 10;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = {x: 0, y: 0, status: true};
+    }
+}
+
 const ballRadius = 25;
 // initial position
 let x = canvas.width / 2;
@@ -15,6 +31,39 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 
 let rightPressed = false;
 let leftPressed = false;
+
+const collisionDetection = () => {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+
+            if (!!b.status) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = false;
+                }
+            }
+        }
+    }
+}
+
+const drawBricks = () => {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (!!bricks[c][r].status) {
+                const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
 
 const drawPaddle = () => {
     ctx.beginPath();
@@ -72,7 +121,9 @@ const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     calcPaddle();
     drawPaddle();
+    drawBricks();
     drawBall();
+    collisionDetection();
     calcDeltas();
 }
 
