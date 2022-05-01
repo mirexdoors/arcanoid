55ab8e3a4739aac5943effeb7a963823ce88c213
Mikhail Prokudin <mirex.doors@mail.ru>
2022-05-01 15:31:33 +0700

    start auth implementation

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import {UsersService} from "../users/users.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private userService: UsersService) {};
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findUser(username);
+
+        if (user && user.password === password) {
+            const { password, ...result } = user;
+            return result;
+        }
+
+        return null;
+    }
+}
